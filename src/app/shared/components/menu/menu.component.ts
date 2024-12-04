96f346674845ef346efffe3cf56c7ca56a255229
doli.mia.fate.s@gmail.com <doli.mia.fate.s@gmail.com>
2024-12-04 10:10:34 +0100

    Navegación por MenuBar

--- src/app/shared/components/menu/menu.component.ts
+++ src/app/shared/components/menu/menu.component.ts
@@ -19,14 +19,17 @@ export class MenuComponent implements OnInit {
                 {
                   label: 'Textos y Fechas',
                   icon: 'pi pi-align-left',
+                  routerLink: '/'
                 },
                 {
                   label: 'Números',
                   icon: 'pi pi-dollar',
+                  routerLink: 'numbers'
                 },
                 {
                   label: 'No Comunes',
                   icon: 'pi pi-globe',
+                  routerLink: 'uncommon'
                 },
               ]
             },
@@ -36,7 +39,7 @@ export class MenuComponent implements OnInit {
               items: [
                 {
                   label: 'Otro Elemento',
-                  icon: 'pi pi-cog',
+                  icon: 'pi pi-cog'
                 }
               ]
             }
