48110f508e2a769b49091510cbaaf2b8eaf2c690
doli.mia.fate.s@gmail.com <doli.mia.fate.s@gmail.com>
2024-12-05 11:18:33 +0100

    JSON y KeyValue Pipes

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -33,5 +33,10 @@ export class UncommonPageComponent {
     this.clients.shift();
   }
 
-
+  // KeyValue Pipe
+  public person = {
+    name: 'Fernando',
+    age: 36,
+    address: 'Ottawa, Canada'
+  }
 }
