c4fff1c297720302b1950a8216a75f42a5719642
doli.mia.fate.s@gmail.com <doli.mia.fate.s@gmail.com>
2024-12-05 08:57:43 +0100

    i18nSelectPipe

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-uncommon-page',
+  selector: 'products-uncommon-page',
   templateUrl: './uncommon-page.component.html',
   styleUrl: './uncommon-page.component.css'
 })
-export class UncommonPageComponent {
 
+export class UncommonPageComponent {
+  // i18n Select
+  public name: string = 'Pablo'
+  public gender: 'male'|'female' = 'male';
+  public invitationMap = {
+    'male': 'invitarlo',
+    'female': 'invitarla'
+  }
+
+  public changeClient(): void {
+    this.name = 'Doli'
+    this.gender = 'female'
+  }
 }
